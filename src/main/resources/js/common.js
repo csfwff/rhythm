bff66ddb3287588b0c13fe9ea33e777eb0e91a5e
adlered <1101635162@qq.com>
2021-09-12 23:38:23 +0800

    :art: manual

--- src/main/resources/js/common.js
+++ src/main/resources/js/common.js
@@ -353,7 +353,7 @@ var Util = {
       return false
     }).bind('keyup', 'Shift+/', function (event) {
       // shift/⇧ ? 新窗口打开键盘快捷键说明文档
-      window.open(Label.servePath + '/about')
+      window.open(Label.servePath + '/article/1631459254239')
       return false
     }).bind('keyup', 'j', function (event) {
       // j 移动到下一项
