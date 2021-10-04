6a6a1770895d6b594b795b7b4e72c7641065118c
adlered <1101635162@qq.com>
2021-10-04 18:35:28 +0800

    :tada: 新游戏：ADarkRoom

--- src/main/java/org/b3log/symphony/processor/TopProcessor.java
+++ src/main/java/org/b3log/symphony/processor/TopProcessor.java
@@ -94,6 +94,7 @@ public class TopProcessor {
         Dispatcher.get("/top/consumption", topProcessor::showConsumption, anonymousViewCheckMidware::handle);
         Dispatcher.get("/top/checkin", topProcessor::showCheckin, anonymousViewCheckMidware::handle);
         Dispatcher.get("/top/online", topProcessor::showOnline, anonymousViewCheckMidware::handle);
+        Dispatcher.get("/top/adr", topProcessor::showADR, anonymousViewCheckMidware::handle);
     }
 
     /**
@@ -195,6 +196,24 @@ public class TopProcessor {
         final Map<String, Object> dataModel = renderer.getDataModel();
         final List<JSONObject> users = activityQueryService.getTopOnlineTimeUsers(Symphonys.TOP_CNT);
         dataModel.put("onlineTopUsers", users);
+
+        dataModelService.fillHeaderAndFooter(context, dataModel);
+        dataModelService.fillRandomArticles(dataModel);
+        dataModelService.fillSideHotArticles(dataModel);
+        dataModelService.fillSideTags(dataModel);
+        dataModelService.fillLatestCmts(dataModel);
+    }
+
+    /**
+     * Shows ADR score ranking list.
+     *
+     * @param context
+     */
+    public void showADR(final RequestContext context) {
+        final AbstractFreeMarkerRenderer renderer = new SkinRenderer(context, "top/adr.ftl");
+        final Map<String, Object> dataModel = renderer.getDataModel();
+        final List<JSONObject> users = activityQueryService.getTopADR(Symphonys.TOP_CNT);
+        dataModel.put("topUsers", users);
 
         dataModelService.fillHeaderAndFooter(context, dataModel);
         dataModelService.fillRandomArticles(dataModel);
--- src/main/java/org/b3log/symphony/service/ActivityQueryService.java
+++ src/main/java/org/b3log/symphony/service/ActivityQueryService.java
@@ -314,4 +314,40 @@ public class ActivityQueryService {
 
         return DateUtils.isSameDay(now, new Date(time));
     }
+
+    /**
+     * Gets the top ADR users (single game max) with the specified fetch size.
+     *
+     * @param fetchSize the specified fetch size
+     * @return users, returns an empty list if not found
+     */
+    public List<JSONObject> getTopADR(final int fetchSize) {
+        final List<JSONObject> ret = new ArrayList<>();
+
+        try {
+            final List<JSONObject> users = userRepository.select("SELECT\n"
+                    + "	u.*, MAX(dataId+0) AS point\n"
+                    + "FROM\n"
+                    + "	" + pointtransferRepository.getName() + " AS p,\n"
+                    + "	" + userRepository.getName() + " AS u\n"
+                    + "WHERE\n"
+                    + "	p.toId = u.oId\n"
+                    + "AND type = 38\n"
+                    + "GROUP BY\n"
+                    + "	toId\n"
+                    + "ORDER BY\n"
+                    + "	point DESC\n"
+                    + "LIMIT ?", fetchSize);
+
+            for (final JSONObject user : users) {
+                avatarQueryService.fillUserAvatarURL(user);
+
+                ret.add(user);
+            }
+        } catch (final RepositoryException e) {
+            LOGGER.log(Level.ERROR, "Gets top eating snake users failed", e);
+        }
+
+        return ret;
+    }
 }
