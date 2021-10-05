76756737ec2090eb7d312e1960def3c4df0acf53
adlered <1101635162@qq.com>
2021-10-05 13:15:27 +0800

    :art: ADR cloud

---
+++ src/main/java/org/b3log/symphony/service/CloudService.java
@@ -0,0 +1,81 @@
+package org.b3log.symphony.service;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.b3log.latke.ioc.Inject;
+import org.b3log.latke.repository.*;
+import org.b3log.latke.service.annotation.Service;
+import org.b3log.symphony.repository.CloudRepository;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CloudService {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(CloudService.class);
+
+    /**
+     * Cloud repository.
+     */
+    @Inject
+    private CloudRepository cloudRepository;
+
+    /**
+     * 上传存档
+     *
+     * @param userId
+     * @param gameId
+     * @param data
+     * @return
+     */
+    synchronized public String sync(final String userId, final String gameId, final String data) {
+        try {
+            final Transaction transaction = cloudRepository.beginTransaction();
+            // 删除旧存档
+            Query cloudDeleteQuery = new Query()
+                    .setFilter(CompositeFilterOperator.and(
+                            new PropertyFilter("userId", FilterOperator.EQUAL, userId),
+                            new PropertyFilter("gameId", FilterOperator.EQUAL, gameId)
+                    ));
+            cloudRepository.remove(cloudDeleteQuery);
+            // 上传新存档
+            JSONObject cloudJSON = new JSONObject();
+            cloudJSON.put("userId", userId)
+                    .put("gameId", gameId)
+                    .put("data", data);
+            String oId = cloudRepository.add(cloudJSON);
+            transaction.commit();
+            return oId;
+        } catch (RepositoryException e) {
+            LOGGER.log(Level.ERROR, "Cannot upload gaming save data to the database.", e);
+            return "";
+        }
+    }
+
+    /**
+     * 获取存档
+     *
+     * @param userId
+     * @param gameId
+     * @return
+     */
+    public String getFromCloud(final String userId, final String gameId) {
+        try {
+            Query cloudQuery = new Query()
+                    .setFilter(CompositeFilterOperator.and(
+                            new PropertyFilter("userId", FilterOperator.EQUAL, userId),
+                            new PropertyFilter("gameId", FilterOperator.EQUAL, gameId)
+                    ));
+            JSONObject result = cloudRepository.getFirst(cloudQuery);
+            return result.optString("data");
+        } catch (RepositoryException e) {
+            return "";
+        }
+    }
+}
