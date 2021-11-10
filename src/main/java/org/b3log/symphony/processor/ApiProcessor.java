5a71ba4a9f7eeecf495e01724c9e152cc74ec42e
adlered <1101635162@qq.com>
2021-11-10 10:08:29 +0800

    :art: API鉴权

--- src/main/java/org/b3log/symphony/processor/ApiProcessor.java
+++ src/main/java/org/b3log/symphony/processor/ApiProcessor.java
@@ -18,21 +18,36 @@
  */
 package org.b3log.symphony.processor;
 
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.b3log.latke.Keys;
 import org.b3log.latke.http.Dispatcher;
+import org.b3log.latke.http.Request;
 import org.b3log.latke.http.RequestContext;
+import org.b3log.latke.http.Response;
 import org.b3log.latke.ioc.BeanManager;
 import org.b3log.latke.ioc.Inject;
 import org.b3log.latke.ioc.Singleton;
+import org.b3log.latke.model.User;
+import org.b3log.latke.service.LangPropsService;
+import org.b3log.latke.service.ServiceException;
+import org.b3log.latke.util.Requests;
+import org.b3log.symphony.model.Common;
+import org.b3log.symphony.model.UserExt;
 import org.b3log.symphony.processor.middleware.CSRFMidware;
 import org.b3log.symphony.processor.middleware.LoginCheckMidware;
 import org.b3log.symphony.processor.middleware.validate.Activity1A0001CollectValidationMidware;
 import org.b3log.symphony.processor.middleware.validate.Activity1A0001ValidationMidware;
 import org.b3log.symphony.service.RewardQueryService;
 import org.b3log.symphony.service.UserQueryService;
+import org.b3log.symphony.util.Sessions;
 import org.b3log.symphony.util.StatusCodes;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 专业团队，专业的 API 接口
@@ -51,6 +66,17 @@ public class ApiProcessor {
     private UserQueryService userQueryService;
 
     /**
+     * Language service.
+     */
+    @Inject
+    private LangPropsService langPropsService;
+
+    /**
+     * 存储用户的Key
+     */
+    public static Map<String, JSONObject> keys = new HashMap<>();
+
+    /**
      * Register request handlers.
      */
     public static void register() {
@@ -60,9 +86,61 @@ public class ApiProcessor {
 
         final ApiProcessor apiProcessor = beanManager.getReference(ApiProcessor.class);
         Dispatcher.get("/api/user/exists/{user}", apiProcessor::userExists);
+        Dispatcher.post("/api/getKey", apiProcessor::getKey);
 
         final RewardQueryService rewardQueryService = beanManager.getReference(RewardQueryService.class);
         Dispatcher.get("/api/article/reward/senders/{aId}", rewardQueryService::rewardedSenders);
+    }
+
+    /**
+     * 通过用户名和密码进行登录，然后发放Key通行证
+     */
+    public void getKey(final RequestContext context) {
+        context.renderJSON(StatusCodes.ERR).renderMsg(langPropsService.get("loginFailLabel"));
+        final JSONObject requestJSONObject = context.requestJSON();
+        final String nameOrEmail = requestJSONObject.optString("nameOrEmail");
+
+        try {
+            JSONObject user = userQueryService.getUserByName(nameOrEmail);
+            if (null == user) {
+                user = userQueryService.getUserByEmail(nameOrEmail);
+            }
+
+            if (null == user) {
+                context.renderMsg(langPropsService.get("notFoundUserLabel"));
+                return;
+            }
+
+            if (UserExt.USER_STATUS_C_INVALID == user.optInt(UserExt.USER_STATUS)) {
+                context.renderMsg(langPropsService.get("userBlockLabel"));
+                return;
+            }
+
+            if (UserExt.USER_STATUS_C_NOT_VERIFIED == user.optInt(UserExt.USER_STATUS)) {
+                context.renderMsg(langPropsService.get("notVerifiedLabel"));
+                return;
+            }
+
+            if (UserExt.USER_STATUS_C_INVALID_LOGIN == user.optInt(UserExt.USER_STATUS)
+                    || UserExt.USER_STATUS_C_DEACTIVATED == user.optInt(UserExt.USER_STATUS)) {
+                context.renderMsg(langPropsService.get("invalidLoginLabel"));
+                return;
+            }
+
+            final String userPassword = user.optString(User.USER_PASSWORD);
+            if (userPassword.equals(requestJSONObject.optString(User.USER_PASSWORD))) {
+                final String key = RandomStringUtils.randomAlphanumeric(32);
+                context.renderCodeMsg(StatusCodes.SUCC, "");
+                context.renderJSONValue("Key", key);
+                keys.put(key, user);
+
+                return;
+            }
+
+            context.renderMsg(langPropsService.get("wrongPwdLabel"));
+        } catch (final ServiceException e) {
+            context.renderMsg(langPropsService.get("loginFailLabel"));
+        }
     }
 
     public void userExists(final RequestContext context) {
