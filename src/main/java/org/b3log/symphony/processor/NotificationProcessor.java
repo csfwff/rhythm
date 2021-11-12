40fda739228a3c1ef865094506309b95780bec8e
adlered <1101635162@qq.com>
2021-11-12 14:11:49 +0800

    :art: API鉴权（用户未读通知计数）

--- src/main/java/org/b3log/symphony/processor/NotificationProcessor.java
+++ src/main/java/org/b3log/symphony/processor/NotificationProcessor.java
@@ -703,7 +703,11 @@ public class NotificationProcessor {
      * @param context the specified context
      */
     public void getUnreadNotificationCount(final RequestContext context) {
-        final JSONObject currentUser = Sessions.getUser();
+        JSONObject currentUser = Sessions.getUser();
+        try {
+            currentUser = ApiProcessor.getUserByKey(context.param("apiKey"));
+        } catch (NullPointerException ignored) {
+        }
         final String userId = currentUser.optString(Keys.OBJECT_ID);
         final Map<String, Object> dataModel = new HashMap<>();
 
