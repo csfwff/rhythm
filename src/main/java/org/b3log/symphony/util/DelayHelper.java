0a25cc45feb27c0796ceef1c8a5dffc2b26d12ba
adlered <1101635162@qq.com>
2022-03-23 15:58:13 +0800

    :art: 主页加入缓存

---
+++ src/main/java/org/b3log/symphony/util/DelayHelper.java
@@ -0,0 +1,33 @@
+package org.b3log.symphony.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DelayHelper {
+
+    private static int times = 0;
+    private static long previousTime = 0L;
+
+    public static void print() {
+        times++;
+        final long timeNow = System.currentTimeMillis();
+        final String timeNowStr = new SimpleDateFormat("mm:ss.SS").format(new Date(timeNow));
+        long timeInterval = timeNow - previousTime;
+        if (timeInterval > 5000 && times != 1) {
+            clear();
+            timeInterval = timeNow - previousTime;
+            times++;
+        }
+        if (times == 1) {
+            timeInterval = 0;
+        }
+        final float timeIntervalSecond = timeInterval / 1000;
+        System.out.println(times + " - " + timeNowStr + " - " + timeIntervalSecond + "s" + " - " + timeInterval + "ms");
+        previousTime = timeNow;
+    }
+
+    public static void clear() {
+        previousTime = 0L;
+        times = 0;
+    }
+}
