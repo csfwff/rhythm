28800a26350c686d1d4ffc73bfd009b803b1f334
adlered <1101635162@qq.com>
2022-04-01 00:42:44 +0800

    :art: 捐助接入支付宝

--- src/main/java/org/b3log/symphony/processor/AlipayProcessor.java
+++ src/main/java/org/b3log/symphony/processor/AlipayProcessor.java
@@ -1,3 +1,21 @@
+/*
+ * Rhythm - A modern community (forum/BBS/SNS/blog) platform written in Java.
+ * Modified version from Symphony, Thanks Symphony :)
+ * Copyright (C) 2012-present, b3log.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.b3log.symphony.processor;
 
 import com.alipay.api.AlipayApiException;
@@ -178,7 +196,7 @@ public class AlipayProcessor {
                     continue;
                 }
                 JSONObject result = query(out_trade_no);
-                if (result.optJSONObject("alipay_trade_query_response").optString("code").equals("10000")) {
+                if (result.optJSONObject("alipay_trade_query_response").optString("trade_status").equals("TRADE_SUCCESS")) {
                     String total_amount = entry.getValue().optString("total_amount");
                     String note = entry.getValue().optString("note");
                     String userId = entry.getValue().optString("userId");
