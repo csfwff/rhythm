33ca45150dddc89281a08f42f8b09ed8774054fd
adlered <1101635162@qq.com>
2022-06-15 22:30:02 +0800

    :art: 系统日志

---
+++ src/main/java/org/b3log/symphony/service/LogsService.java
@@ -0,0 +1,41 @@
+/*
+ * Rhythm - A modern community (forum/BBS/SNS/blog) platform written in Java.
+ * Modified version from Symphony, Thanks Symphony :)
+ * Copyright (C) 2012-present, b3log.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.b3log.symphony.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.b3log.latke.ioc.Inject;
+import org.b3log.latke.service.annotation.Service;
+import org.b3log.symphony.processor.LogsProcessor;
+
+/**
+ * @author fangcong
+ * @version 0.0.1
+ * @since Created by work on 2022-02-12 22:58
+ **/
+@Service
+public class LogsService {
+
+    private static final Logger LOGGER = LogManager.getLogger(LogsService.class);
+
+    @Inject
+    private LogsProcessor logsProcessor;
+
+
+}
