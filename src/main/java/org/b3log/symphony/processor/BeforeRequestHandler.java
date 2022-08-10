ecb2ddfc253ea90a7028b97a0c38112bfb37328b
adlered <1101635162@qq.com>
2022-08-10 14:20:57 +0800

    :art: anti cc rule change

--- src/main/java/org/b3log/symphony/processor/BeforeRequestHandler.java
+++ src/main/java/org/b3log/symphony/processor/BeforeRequestHandler.java
@@ -61,7 +61,7 @@ public class BeforeRequestHandler implements Handler {
      */
     private static final Logger LOGGER = LogManager.getLogger(BeforeRequestHandler.class);
 
-    SimpleCurrentLimiter antiCCLimiter = new SimpleCurrentLimiter(5, 5);
+    SimpleCurrentLimiter antiCCLimiter = new SimpleCurrentLimiter(15, 15);
 
     @Override
     public void handle(final RequestContext context) {
