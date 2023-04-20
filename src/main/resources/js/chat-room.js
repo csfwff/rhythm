10b10a4f1b1c7a6688bd8544f1ae02096bb7dd56
陈辉 <1101635162@qq.com>
2023-04-20 16:36:48 +0800

    :art: 懒得写，自己看吧

--- src/main/resources/js/chat-room.js
+++ src/main/resources/js/chat-room.js
@@ -539,13 +539,17 @@ border-bottom: none;
                     }
                     $("#si-guo-list").prepend(`
     <li class="fn__flex menu__item">
-        <img class="avatar avatar--mid" style="width: 24px; height: 24px; margin-right: 10px; background-image: none; background-color: transparent; filter: blur(1px);" src="` + userAvatarURL + `">
+        <img class="avatar avatar--mid" style="width: 24px; height: 24px; margin-right: 10px; background-image: none; background-color: transparent;" src="` + userAvatarURL + `">
         <div class="fn__flex-1" style="text-align: left !important;">
             <h2 class="list__user">
                 <a target="_blank" href="` + Label.servePath + `/member/` + userName + `" style="color: #c0c0c0; text-decoration: none;">` + useName + `</a>
             </h2>
+            <span class="ft__fade ft__smaller"><a onclick="Util.closeAlert(this);ChatRoom.editor.setValue('合议破戒 ` + userName + `');ChatRoom.send();$(window).scrollTop(0);" style="cursor: pointer; font-weight: bold;" href="javascript:void(0);">爲他求情</a></span>
         </div>
-        <div class="fn__flex-center" style="color: #ff1919; font-weight: bold">將於 ` + date.getFullYear() + `年` + (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + `月` + date.getDate() + `日 ` + date.getHours() + `時` + date.getMinutes() + `分 釋放</div>
+        <div class="fn__flex-center" style="color: #ff1919; font-weight: bold">
+        將於 ` + date.getFullYear() + `年` + (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + `月` + date.getDate() + `日 ` + date.getHours() + `時` + date.getMinutes() + `分 釋放
+        </div>
+  
     </li>
                 `);
                 }
