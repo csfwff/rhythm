0d3ae5be33c997a36fb8398a32e97748405be1d3
陈辉 <1101635162@qq.com>
2023-04-27 16:25:13 +0800

    :wrench:

--- src/main/java/org/b3log/symphony/service/CronMgmtService.java
+++ src/main/java/org/b3log/symphony/service/CronMgmtService.java
@@ -207,13 +207,23 @@ public class CronMgmtService {
         Symphonys.SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(() -> {
             try {
                 cacheMgmtService.refreshIndexCache();
-                articleQueryService.refreshHotArticlesCache();
             } catch (final Exception e) {
                 LOGGER.log(Level.ERROR, "Executes cron failed", e);
             } finally {
                 Stopwatchs.release();
             }
         }, delay, 60 * 1000, TimeUnit.MILLISECONDS);
+        delay += 2000;
+
+        Symphonys.SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(() -> {
+            try {
+                articleQueryService.refreshHotArticlesCache();
+            } catch (final Exception e) {
+                LOGGER.log(Level.ERROR, "Executes cron failed", e);
+            } finally {
+                Stopwatchs.release();
+            }
+        }, delay, 15 * 60 * 1000, TimeUnit.MILLISECONDS);
         delay += 2000;
 
         Symphonys.SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(() -> {
