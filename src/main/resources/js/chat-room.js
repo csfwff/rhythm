00f50c6461bc05018fbb9966ee6912f21935fb17
陈辉 <1101635162@qq.com>
2023-05-22 13:36:49 +0800

    :art: 聊天引用跳转

--- src/main/resources/js/chat-room.js
+++ src/main/resources/js/chat-room.js
@@ -1329,7 +1329,7 @@ border-bottom: none;
                     md = md.replace(/\n/g, "\n> ");
                 }
             });
-            ChatRoom.editor.insertValue(`\n##### 引用 @"${userName} <a href="${Label.servePath}/cr#chatroom${id}">↩</a>"  \n> "${md}"</span>\n`, !1);
+            ChatRoom.editor.insertValue(`\n##### 引用 @${userName} [↩](${Label.servePath}/cr#chatroom${id} "跳转至原消息")  \n> ${md}</span>\n`, !1);
         }
         $(window).scrollTop(0);
     },
