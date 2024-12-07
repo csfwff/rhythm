07efaf08545e4dddc2a48b0718fa66793eeacc7f
陈辉 <1101635162@qq.com>
2024-12-07 23:07:07 +0800

    :art: rhyus beta1

--- src/main/java/org/b3log/symphony/util/NodeUtil.java
+++ src/main/java/org/b3log/symphony/util/NodeUtil.java
@@ -1,3 +1,21 @@
+/*
+ * Rhythm - A modern community (forum/BBS/SNS/blog) platform written in Java.
+ * Modified version from Symphony, Thanks Symphony :)
+ * Copyright (C) 2012-present, b3log.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.b3log.symphony.util;
 
 import org.apache.logging.log4j.Level;
@@ -175,7 +193,10 @@ public class NodeUtil {
                 WebSocket webSocket = client.newWebSocketBuilder()
                         .buildAsync(URI.create(serverUri), new WebSocketListener(responseFuture))
                         .join();
-                webSocket.sendText(Symphonys.get("chatroom.node.adminKey") + ":::push " + result, true);
+
+                String message = Symphonys.get("chatroom.node.adminKey") + ":::push " + result;
+                webSocket.sendText(message, true);
+
                 try {
                     String response = responseFuture.get(10, TimeUnit.SECONDS);
                     webSocket.sendClose(1000, "Nornal Closure");
